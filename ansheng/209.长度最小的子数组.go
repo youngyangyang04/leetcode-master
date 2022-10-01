970b2fbb9ae529da4fe5f30bcad696b9194d6a69
victoryann <victoryanns163.com>
2022-10-01 14:43:11 +0800

    leetcode 209

---
+++ ansheng/209.长度最小的子数组.go
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=golang
+ *
+ * [209] 长度最小的子数组
+ *
+ * https://leetcode.cn/problems/minimum-size-subarray-sum/description/
+ *
+ * algorithms
+ * Medium (47.88%)
+ * Likes:    1393
+ * Dislikes: 0
+ * Total Accepted:    425K
+ * Total Submissions: 887.7K
+ * Testcase Example:  '7\n[2,3,1,2,4,3]'
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ *
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr]
+ * ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 1
+ * 1
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+ *
+ *
+ */
+
+// @lc code=start
+func minSubArrayLen(target int, nums []int) int {
+	start, length := 0, 0
+	min := len(nums) + 10
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		length++
+		if sum >= target {
+			for sum >= target {
+				sum -= nums[start]
+				start++
+				length--
+			}
+			if length+1 < min {
+				min = length + 1
+			}
+		}
+	}
+	if min == len(nums)+10 {
+		return 0
+	}
+	return min
+}
+
+// @lc code=end
+
