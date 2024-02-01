2e58e7045c37bdcf8acccc595781b3bdbd39deda
Wenting <61220533+wentingluo1993@users.noreply.github.com>
2024-02-01 13:42:49 -0700

    Add files via upload

---
+++ 代码随想录29/Day38/509_Fibonacci_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
---
+++ 代码随想录29/Day38/746_Min_cost_climbing_stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        if n == 0:
+            return 0
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 0
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[n]
