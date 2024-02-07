7406257f7647a26839c8d11460985043b8a49326
Wenting <61220533+wentingluo1993@users.noreply.github.com>
2024-02-07 15:12:00 -0700

    Add files via upload

---
+++ 代码随想录29/Day44/518_Coin_change_ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for j in range(coin, amount + 1):
+                dp[j] += dp[j - coin]
+        return dp[amount]
